7f8c41d62f9d09b9a369e837740668c7a8ba0932
Sara <sara.szajewska@gmail.com>
2016-05-24 12:27:05 +0200

    Resultatdatabas

--- RiggenPoker/RiggenPoker/Models/IdentityModels.cs
+++ RiggenPoker/RiggenPoker/Models/IdentityModels.cs
@@ -25,6 +25,8 @@ namespace RiggenPoker.Models
         {
         }
 
+        public DbSet<Result> Results { get; set; }
+
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
---
+++ RiggenPoker/RiggenPoker/Models/Result.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RiggenPoker.Models
+{
+    public class Result
+    {
+        public int Id { get; set; }
+        public DateTime? Date { get; set; }
+        public int History { get; set; }
+        public int TotalScore { get; set; }
+        public int NewScore { get; set; }
+        public virtual ICollection<ApplicationUser> UserName { get; set; }
+    }
+}
